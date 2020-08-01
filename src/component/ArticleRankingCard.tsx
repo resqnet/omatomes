06109cd98c5d80eb64efb5806c72f4cc186fbd32
resqnet <resqwork@gmail.com>
2020-08-02 03:59:37 +0900

    modified: components are now modular.

---
+++ src/component/ArticleRankingCard.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+
+const ArticleRankingCard: React.FC = () => {
+  return (
+    <>
+      <Root>asdf</Root>
+    </>
+  );
+};
+
+const Root = styled.div`
+  width: 100%;
+  height: 64px;
+  display: flex;
+  align-items: center;
+`;
+
+export default ArticleRankingCard;
---
+++ src/component/Footer.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+
+const Footer: React.FC = () => {
+  return (
+    <>
+      <Root>asdf</Root>
+    </>
+  );
+};
+
+const Root = styled.div`
+  width: 100%;
+  height: 64px;
+  display: flex;
+  align-items: center;
+`;
+
+export default Footer;

1495106888cad8a98eea6f7dad00c16cb6aeafe3
resqnet <resqwork@gmail.com>
2020-08-02 03:58:01 +0900

    modified: typescript setting for storybook

---
+++ src/__stories__/Header.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Header from "./../component/Header";
+
+export default {
+  title: "Header",
+  component: Header,
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const Text = () => <Header />;
